35b13f12bac03c5ca2b54e3521e40059239f316c
zachkaufman36 <35308366+zachkaufman36@users.noreply.github.com>
2025-01-18 06:59:35 -0500

    Added github link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Zach Kaufman Online Section Spring 2025</h1>
+      <h1>Zachary Kaufman</h1><b /> 
+      <h1>Online Section Spring 2025</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
@@ -16,5 +17,6 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />}/>
         <Route path="Lab3" element={<Lab3 />}/>
       </Routes>
+      <a href="https://github.com/zachkaufman36/kanbas-react-web-app">Link to My Github</a>
     </div>
 );}
