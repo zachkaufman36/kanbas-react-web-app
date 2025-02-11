e7077e45bc9c114a8ed00e8c46d4203267648788
zachkaufman36 <35308366+zachkaufman36@users.noreply.github.com>
2025-02-11 14:02:52 -0500

    Added console logs

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -34,6 +34,7 @@ import PathParameters from "./PathParameters";
 
 
 export default function Lab3() {
+    console.log('Hello World!');
     return (
         <div id="wd-lab3">
              <h2>Lab 3</h2>
