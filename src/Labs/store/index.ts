03119b005aac189a0e291f118cdc4b0a1a6dc8be
zachkaufman36 <35308366+zachkaufman36@users.noreply.github.com>
2025-03-12 13:46:15 -0400

    Cleaning up errors found on compile

--- src/Labs/store/index.ts
+++ src/Labs/store/index.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "../Lab4/ReduxExamples/AddRedux/addReducer";
-import todosReducer from "../Lab4/ReduxExamples/todos/TodosReducer";
+import todosReducer from "../Lab4/ReduxExamples/todos/TodoReducer";
 const store = configureStore({
   reducer: { helloReducer, counterReducer, addReducer, todosReducer }});
 export default store;
